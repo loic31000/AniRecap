2d988ba27febcd4d1aaeefcc20bb89a0906bb400
=loic31000 <leclairloic1@gmail.com>
2026-07-02 11:29:58 +0200

    Add CatalogueController and css, add layout (component navbar, header), oeuvreCard et searchCard

---
+++ src/Twig/Components/Header.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class Header
+{
+    use DefaultActionTrait;
+
+    public ?string $backRoute = null;
+    public ?string $title = 'AniRecap';
+}
---
+++ src/Twig/Components/Navbar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class Navbar
+{
+    use DefaultActionTrait;
+
+    public ?string $active = null;
+}
